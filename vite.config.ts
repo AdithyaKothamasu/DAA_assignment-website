bdb28e2dd6855de9e3c35073822ead187d383dc4
Sai Adithya Kothamasu <adithya261004@gmail.com>
2025-03-23 00:24:35 +0530

    some changes made

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/project/', // Add this line - should match your repo name
+  base: './',
+  build: {
+    assetsDir: 'assets',
+    emptyOutDir: true,
+    sourcemap: false,
+  }
 })
